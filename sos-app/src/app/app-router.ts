debf1dc8b8b370692e83f5b21b77a91f567f232f
Swasti Maity <166163904+SwastiMaity@users.noreply.github.com>
2025-09-14 19:06:13 +0000

    Refactor routing configuration and update navigation links in the application

--- sos-app/src/app/app-router.ts
+++ sos-app/src/app/app-router.ts
@@ -1,8 +1,20 @@
+
 import { Routes } from '@angular/router';
 import { SosButton } from './sos-button/sos-button';
 import { Responder } from './responder/responder';
 
 export const routes: Routes = [
-  { path: '', component: SosButton },        // victim page
-  { path: 'responder', component: Responder } // responder page
+  {
+    path: '',
+    component: SosButton,
+    pathMatch: 'full' // default route
+  },
+  {
+    path: 'responder',
+    component: Responder
+  },
+  {
+    path: '**',
+    redirectTo: '' // fallback to SOS Button
+  }
 ];
